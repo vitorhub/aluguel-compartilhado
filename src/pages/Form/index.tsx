a94c1999d8087265072806b8c235cb688b3a5f71
Vitor Falcao <joaofalcao.33@outlook.com>
2023-04-24 16:51:32 -0300

    adding api state and district

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function Form() {
+
+    const [estado, setEstado] = useState([])
+    const [data, setData] = useState([])
+
+    // fetch(`https://viacep.com.br/ws/${e}/json/`) // para fetch
+    function getDistritos(e: any) {
+        e.preventDefault();
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/municipios`)
+            .then((response) => response.json())
+            .then((data) => { console.log(data); setData(data) })
+            .catch((err) => console.log(err))
+    }
+    function getEstados(e: any) {
+        setEstado(e)
+        console.log(e)
+    }
+    function percorreData() {
+
+    }
+
     return (
         <>
             <form action="">
                 <label htmlFor="availableValue">Valor disponível</label>
                 <input type="number" name="availableValue" /> <br />
 
-                <label htmlFor="region">Escolha região</label>
-                <input type="text" name="region" id="region" placeholder='Cidade Estado Bairro'/> <br />
+                <label htmlFor="estado">Escolha Estado</label>
+                <select name="estado" id="estado" onChange={(e) => getEstados(e.target.value)}>
+                    {<option value={"RS"}>{"RS"} </option>}
+                    {<option value={"SP"}>{"SP"} </option>}
+                    {<option value={"RJ"}>{"RJ"} </option>}
+                </select>
+                <select name="municipio" id="municipio" /* onChange={(e) => getEstados(e.target.value)} */ >
+                    {data.length > 0 ?
+                        data.map( (x: any,y) => <option key={y}>{ x.nome  }</option> )
+                        // <option value={"uva"}>{"uva"}</option>
+                        : <option value={"pera"}>{"pera"}</option>}
+                </select>
+
+
+                <button onClick={getDistritos} > getDistritos </button> <br />
 
                 <label htmlFor="npeople">Com quantas pessoas divide o aluguel</label>
                 <input type="number" name="npeople" id="npeople" /> <br />
@@ -22,12 +56,12 @@ export default function Form() {
                 <input type="checkbox" name="temmobilia" id="temmobilia" /> <br />
 
                 <label htmlFor="apresentacao">Apresente-se</label> <br />
-                <textarea name="apresentacao" id="apresentacao" cols={30} rows={10}>
-                    Informe se tem pet, que mobílias já tem, informações que facilitem para a outra pessoa
-                </textarea>
+                <textarea name="apresentacao" id="apresentacao" cols={30} rows={10}
+                    defaultValue={'Informe se tem pet, que mobílias já tem, informações que facilitem para a outra pessoa'}
+                />
 
             </form>
-            
+
             <Link to="/"> <button> volta raiz </button> </Link>
         </>
     )
